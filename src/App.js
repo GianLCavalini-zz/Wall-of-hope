bab55e5ec2941053b1de6696cf944690910d3b69
Leonardo Araujo <leo.san9@hotmail.com>
2022-04-21 18:12:22 -0300

    Create OK, Read OK, Update OK

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Editar } from "./Components/Pages/Editar/Editar";
 import { Home } from "./Components/Pages/Home/Home";
+import { Instituição } from "./Components/Pages/Instituição/instituição";
 import { Register } from "./Components/Pages/Register/Register";
 
 
@@ -10,6 +12,8 @@ import { Register } from "./Components/Pages/Register/Register";
     <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/register" element={<Register/>} />
+    <Route path="/instituicao/:id" element={<Instituição/>}/>
+    <Route path="/editar-cadastro/:id" element={<Editar/>}/>
     </Routes>
     </BrowserRouter>
     </div>
--- src/Components/Pages/Card/Card.js
+++ src/Components/Pages/Card/Card.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./card.css"
+import { Link } from "react-router-dom";
 
 export function Card() {
     const [inst, setInst] = useState([])
@@ -15,6 +16,17 @@ export function Card() {
         getInst()
         
     }, []);
+
+    useEffect(()=>{
+        async function getInst(){
+            const response = await axios.get(
+                `https://ironrest.herokuapp.com/helpinther/`,
+            );
+            setInst(response.data[Math.floor(Math.random() * response.data.length)])
+        }
+        getInst()
+        
+    }, []);
     console.log(inst)
     return ( 
 
@@ -22,6 +34,9 @@ export function Card() {
             <img  src={inst.img} alt={inst.Instituição} style={{maxWidth: "300px", minWidth: "300px", borderRadius: "5%"}} />
             <h1>{inst.Instituição}</h1>
             <p>{inst.Descrição}</p>
+            <Link to={`/instituicao/${inst._id}`} target="_blank"><button>i</button></Link>
+            <button>{"<3"}</button>
+            <button>X</button>
         </div>
      );
 }
